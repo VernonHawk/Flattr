723b57f3a5d3ecf403877e0e84d2aa256166b983
Igor Morenec <Igor.Morenec@gmail.com>
2019-11-17 20:01:30 +0200

    Add header text
    Also disable a useless tslint rule

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,14 @@
-import { Container, CssBaseline, Typography } from '@material-ui/core'
+import { Container, CssBaseline, Grid, Typography } from '@material-ui/core'
 import React from 'react'
 
-const App = (): JSX.Element => {
-  return (
-    <Container maxWidth='md'>
-      <CssBaseline />
-      <Typography>Hello world!</Typography>
-    </Container>
-  )
-}
+const App = (): JSX.Element => (
+  <Container maxWidth='md'>
+    <CssBaseline />
+    <Grid container direction='column' justify='center' alignItems='center'>
+      <Typography variant='h1'>Flattr</Typography>
+      <Typography variant='h3'>Get a fair price for your flat!</Typography>
+    </Grid>
+  </Container>
+)
 
 export default App
